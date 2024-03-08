5c28081d74673a9753fd65c2b6713c37ab6f3fb4
callaito <radelinrsm029@gmail.com>
2024-03-07 20:20:32 -0400

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Numeros positivos");
+Console.WriteLine();
+
+/*
+ Leer un numero entero positivo y cuando sea negativo, el bucle debe parar.
+Mostrar un mensaje que diga que ha introducido un numero negativo.
+ */
+
+int numero;
+
+Console.Write("Introduzca un numero entero positivo: ");
+numero=int.Parse(Console.ReadLine());
+
+while(numero > 0)
+{
+    Console.Write("Introduzca un numero entero positivo: ");
+    numero = int.Parse(Console.ReadLine());
+
+}
+
+Console.WriteLine("Ha introducido un numero negativo.");
